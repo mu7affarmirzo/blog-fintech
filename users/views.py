a6bb16e3958905f3a997d332c0c54d8bc479ec7d
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2022-03-25 09:30:04 +0500

    test func added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 
 class UsersDetailView(TemplateView):
     template_name = 'detail.html'
+
+def tester_func(request):
+    return HttpResponse('This is a test')
